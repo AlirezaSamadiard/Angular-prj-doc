59cb011ef422c84cdbea234d1eb8ccfaf9f48b55
AlirezaSamadiard <113842809+AlirezaSamadiard@users.noreply.github.com>
2023-09-02 11:25:22 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-first
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/jjn4cr)
---
+++ src/app/routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+const routeConfig: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    title: 'Home details',
+  },
+];
+
+export default routeConfig;
